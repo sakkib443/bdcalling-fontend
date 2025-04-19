cf644780ffae1c45f5fbda0119aae4737f80ddcf
sakibbdcallingit <mahabuburbdcalling@gmail.com>
2025-04-19 13:56:23 +0600

    add

--- src/Page/Contact/Contact.jsx
+++ src/Page/Contact/Contact.jsx
@@ -29,13 +29,27 @@ const Contact = () => {
                     </div>
                 </div>
             </div>
-           
+
             <div className="flex flex-col lg:flex-row gap-8 lg:w-8/12 mx-auto">
 
                 <DropQuestion></DropQuestion>
                 <Address></Address>
             </div>
             <FAQ></FAQ>
+
+
+            <div className="w-full flex justify-center py-10">
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d8662.770468055722!2d90.41722969357912!3d23.765697600000006!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755c754583dd209%3A0xdd0c5fcc7d2d3836!2sbdCalling%20IT%20Ltd.%20-%20Corporate%20Office!5e1!3m2!1sen!2sbd!4v1744982086149!5m2!1sen!2sbd"
+                    width="50%"
+                    height="450"
+                    style={{ border: 0 }}
+                    allowFullScreen
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                    title="BDCalling IT Ltd. Location"
+                />
+            </div>
         </div>
     );
 };
